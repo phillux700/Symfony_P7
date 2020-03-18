0a5f61ca1f7f0d02b5b15a6721876c348ba47508
sergisergio <ptraon@gmail.com>
2020-03-18 16:58:30 +0100

    upgrade symfony from 4.2 to 4.3

--- src/Repository/ClientRepository.php
+++ src/Repository/ClientRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Client;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 
 /**
@@ -15,7 +15,7 @@ use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
  */
 class ClientRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Client::class);
     }
